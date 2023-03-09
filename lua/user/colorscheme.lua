6e31e376e8c42b7d3e8efe43c31ae72d886fe929
Taylor Flagg <trflagg@gmail.com>
2023-03-09 12:29:50 -0600

    Use light "one" colorscheme by default

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,6 @@
-local colorscheme = "tokyonight-night"
+local colorscheme = "one"
+
+vim.o.background='light'
 
 local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not status_ok then
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -63,6 +63,8 @@ return packer.startup(function(use)
   -- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
+  use { 'rakr/vim-one' }
+  use { 'tanvirtin/monokai.nvim' }
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
@@ -105,7 +107,6 @@ return packer.startup(function(use)
   -- Taylor
   use {'junegunn/goyo.vim'}
   use { "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons" }
-  use { 'tanvirtin/monokai.nvim' }
 
   -- end Taylor
 
